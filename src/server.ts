e5252638460442417fde401c817b45861808b26c
kftlfd <kftlfd@ex.ua>
2026-04-11 15:26:57 +0300

    WIP: reorganize bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
-import Fastify from 'fastify';
-
 import { createRedisCache } from '@/cache/redisCache.js';
 import { config } from '@/config/config.js';
 import { env } from '@/config/env.js';
@@ -15,107 +13,122 @@ import { createRepoSubscriptionsQueue } from '@/queue/repo-subscriptions/repo-su
 import { createRepoSubscriptionsWorker } from '@/queue/repo-subscriptions/repo-subscriptions.worker.js';
 import { redis } from '@/redis/redis.js';
 import { createRepositoryRepo } from '@/repository/repository.repo.js';
-import { createScannerLoop } from '@/scanner/scanner.service.js';
+import { createScannerService } from '@/scanner/scanner.service.js';
+import { createFastifyServer } from '@/server/server.js';
 import { createSubscriptionController } from '@/subscription/subscription.controller.js';
 import { createSubscriptionRepo } from '@/subscription/subscription.repo.js';
 import { createSubscriptionService } from '@/subscription/subscription.service.js';
 import { createTokenRepo } from '@/token/token.repo.js';
 import { createTokenService } from '@/token/token.service.js';
 
-const logger = createLogger();
+function createApp() {
+  // infra
+  const logger = createLogger();
+  const cache = createRedisCache(redis);
 
-const confirmationEmailsQueue = createConfirmationEmailsQueue({
-  config: config.queues.confirmationEmails.queue,
-  redis,
-});
-const releaseNotificationsQueue = createReleaseNotificationsQueue({
-  config: config.queues.releaseNotifications.queue,
-  redis,
-});
-const repoSubscriptionsQueue = createRepoSubscriptionsQueue({
-  config: config.queues.repoSubscriptions.queue,
-  redis,
-});
-
-const cache = createRedisCache(redis);
-const repositoryRepo = createRepositoryRepo({ config: config.repositoryRepo, logger, cache });
-const subscriptionRepo = createSubscriptionRepo();
-const tokenRepo = createTokenRepo();
-const emailService = createEmailService();
-const tokenService = createTokenService({ config: config.tokenService, tokenRepo });
-const ghClient = createGithubClient(config.githubClient);
-const cachedGhClient = createCachedGithubClient({
-  config: config.githubClient,
-  base: ghClient,
-  cache,
-  logger,
-});
-const subscriptionService = createSubscriptionService({
-  logger,
-  repositoryRepo,
-  subscriptionRepo,
-  tokenService,
-  githubClient: cachedGhClient,
-  confirmationEmailsQueue,
-});
-
-const subsController = createSubscriptionController(subscriptionService);
-
-const startScannerLoop = createScannerLoop({
-  config: config.scanner,
-  logger,
-  repositoryRepo,
-  githubClient: cachedGhClient,
-  repoSubscriptionsQueue,
-});
-
-const createWorkers = () => [
-  createConfirmationEmailsWorker({
-    redis,
-    config: config.queues.confirmationEmails.worker,
+  // clients
+  const ghClient = createGithubClient(config.githubClient);
+  const cachedGhClient = createCachedGithubClient({
+    config: config.githubClient,
+    base: ghClient,
+    cache,
     logger,
-    emailService,
-  }),
-  createReleaseNotificationsWorker({
+  });
+
+  // repos
+  const repositoryRepo = createRepositoryRepo({ config: config.repositoryRepo, logger, cache });
+  const subscriptionRepo = createSubscriptionRepo();
+  const tokenRepo = createTokenRepo();
+
+  // queues
+  const confirmationEmailsQueue = createConfirmationEmailsQueue({
+    config: config.queues.confirmationEmails.queue,
     redis,
-    config: config.queues.releaseNotifications.worker,
-    logger,
-    emailService,
-    repositoryRepo,
-  }),
-  createRepoSubscriptionsWorker({
+  });
+  const releaseNotificationsQueue = createReleaseNotificationsQueue({
+    config: config.queues.releaseNotifications.queue,
     redis,
-    config: config.queues.repoSubscriptions.worker,
-    jobConfig: config.queues.repoSubscriptions.job,
+  });
+  const repoSubscriptionsQueue = createRepoSubscriptionsQueue({
+    config: config.queues.repoSubscriptions.queue,
+    redis,
+  });
+
+  // services
+  const tokenService = createTokenService({ config: config.tokenService, tokenRepo });
+  const subscriptionService = createSubscriptionService({
     logger,
     repositoryRepo,
     subscriptionRepo,
-    releaseNotificationsQueue,
-  }),
-];
+    tokenService,
+    githubClient: cachedGhClient,
+    confirmationEmailsQueue,
+  });
+  const emailService = createEmailService();
+  const scannerService = createScannerService({
+    config: config.scanner,
+    logger,
+    repositoryRepo,
+    githubClient: cachedGhClient,
+    repoSubscriptionsQueue,
+  });
 
-const app = Fastify({
-  logger,
-});
+  // queue workers
+  const workers = [
+    createConfirmationEmailsWorker({
+      redis,
+      config: config.queues.confirmationEmails.worker,
+      logger,
+      emailService,
+    }),
+    createReleaseNotificationsWorker({
+      redis,
+      config: config.queues.releaseNotifications.worker,
+      logger,
+      emailService,
+      repositoryRepo,
+    }),
+    createRepoSubscriptionsWorker({
+      redis,
+      config: config.queues.repoSubscriptions.worker,
+      jobConfig: config.queues.repoSubscriptions.job,
+      logger,
+      repositoryRepo,
+      subscriptionRepo,
+      releaseNotificationsQueue,
+    }),
+  ];
 
-app.get('/', function handler() {
-  return { hello: 'world' };
-});
+  // server
+  const subscriptionController = createSubscriptionController(subscriptionService);
+  const app = createFastifyServer({ logger, subscriptionController });
 
-app.register(subsController, { prefix: '/api' });
+  return {
+    app,
+    scannerService,
+    workers,
+    logger,
+  };
+}
 
-async function bootstrap() {
-  await startScannerLoop();
+function bootstrap() {
+  const { app, scannerService, logger } = createApp();
 
-  createWorkers();
+  async function start() {
+    return Promise.all([
+      scannerService.start(),
 
-  await app.listen({
-    host: env.NODE_ENV === 'dev' ? '127.0.0.1' : '0.0.0.0',
-    port: 3000,
+      app.listen({
+        host: env.NODE_ENV === 'dev' ? '127.0.0.1' : '0.0.0.0',
+        port: 3000,
+      }),
+    ]);
+  }
+
+  start().catch((err: unknown) => {
+    logger.error(err, 'Bootstrap fail');
+    process.exit(1);
   });
 }
 
-bootstrap().catch((err: unknown) => {
-  logger.error(err, 'Bootstrap fail');
-  process.exit(1);
-});
+bootstrap();

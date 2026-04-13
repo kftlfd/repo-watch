39e0b903b0589e9d8e860fe87e27a775c9b66707
kftlfd <kftlfd@ex.ua>
2026-04-13 12:42:38 +0300

    install pino-pretty

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,7 +1,18 @@
+import type { PrettyOptions } from 'pino-pretty';
 import pino from 'pino';
+
+import { env } from '@/config/env.js';
 
 export type Logger = pino.Logger;
 
 export function createLogger(): Logger {
-  return pino();
+  return pino({
+    transport:
+      env.NODE_ENV === 'dev'
+        ? {
+            target: 'pino-pretty',
+            options: { colorize: true } satisfies PrettyOptions,
+          }
+        : undefined,
+  });
 }

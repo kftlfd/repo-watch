bf64d9d58b2e2d824cc4703e06563cecf5a1131b
kftlfd <kftlfd@ex.ua>
2026-05-02 14:55:02 +0300

    upd token repo

--- src/token/token.repo.ts
+++ src/token/token.repo.ts
@@ -1,23 +1,15 @@
 import { and, eq, gt } from 'drizzle-orm';
 import { err, ok, ResultAsync } from 'neverthrow';
 
-import type { DBError, DBNotFoundError } from '@/db/errors.js';
 import { db } from '@/db/client.js';
 import { dbErrors } from '@/db/errors.js';
 import { tokens } from '@/db/schema.js';
 
 export type Token = typeof tokens.$inferSelect;
 export type NewToken = typeof tokens.$inferInsert;
-export type TokenType = 'confirm' | 'unsubscribe';
+export type TokenType = Token['type'];
 
-export type TokenRepo = {
-  create(data: NewToken): ResultAsync<Token, DBError>;
-  getValidByHashAndType(
-    tokenHash: string,
-    type: TokenType,
-  ): ResultAsync<Token, DBError | DBNotFoundError>;
-  deleteById(id: number): ResultAsync<void, DBError>;
-};
+export type TokenRepo = ReturnType<typeof createTokenRepo>;
 
 function create(data: NewToken) {
   return ResultAsync.fromPromise(db.insert(tokens).values(data).returning(), (e) =>
@@ -50,7 +42,7 @@ function deleteById(id: number) {
   ).map(() => {});
 }
 
-export function createTokenRepo(): TokenRepo {
+export function createTokenRepo() {
   return {
     create,
     getValidByHashAndType,
